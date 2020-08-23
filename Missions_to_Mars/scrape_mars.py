966a3b7ed15f8ecc67ee8759386082365b5cd414
Colby Hoke <choke@redaht.com>
2020-08-22 23:23:36 -0400

    finally, but needs styling

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -35,7 +35,7 @@ def init_browser():
 def scraper():
     
     ###############################################################
-    # Scrape the NASA Mars News Site:
+    # NASA Mars News Site:
     # https://mars.nasa.gov/news
 
     # Collect the latest News Title and Paragraph Text.
@@ -57,12 +57,12 @@ def scraper():
     soup = BeautifulSoup(html, 'html.parser')
 
     article = soup.find('div', class_='list_text')
-
+    
     news_title = article.find('a').text
     news_p = article.find('div', class_='article_teaser_body').text
 
     ###############################################################
-    # Get the JPL Featured Space Image
+    # JPL Featured Space Image
     # https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars
     #
     # Find the full size image url for the current Featured Mars Image
@@ -76,20 +76,15 @@ def scraper():
 
     browser.visit(space_img_url)
 
-    time.sleep(1)
-
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
     img_link = soup.find('section', class_='main_feature').find('a', class_='button fancybox')['data-link']
     img_page_url = url_split[0] + img_link
 
-
     # Visit that page and parse the HTML
     browser.visit(img_page_url)
     
-    time.sleep(1)
-
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -101,7 +96,7 @@ def scraper():
     featured_image_url = 'https:' + hires_image_url
 
     ###############################################################
-    # Get Mars facts
+    # Mars facts
     # https://space-facts.com/mars/
     # 
     # Use pandas to read tables
@@ -121,22 +116,65 @@ def scraper():
     mars_df = tables[0]
 
     # Give columns names and reset the index
-    mars_df.columns=['Paramater','Value']
-    mars_df.set_index('Paramater', inplace=True)
+    mars_df.columns=['Description','Mars']
+    mars_df.set_index('Description', inplace=True)
 
     # Convert the table to HTML and strip the new line returns
     mars_html_table = mars_df.to_html()
-    mars_html_table = mars_html_table.replace('\n', '') 
+    mars_html_table = mars_html_table.replace('\n', '')
 
     ###############################################################
-    # For the future, if this site unfucks itself
+    # Mars hemispheres images
+    # 
     ###############################################################
+    usgs_astro_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 
-    #usgs_astro_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Marshere'
+    # Split the URL to use later
+    usgs_astro_url_split = usgs_astro_url.split('/search',1)
 
-    #browser.visit(usgs_astro_url)
-    #html = browser.html
-    #soup = BeautifulSoup(html, 'html.parser')
+    # Visit the page
+    browser.visit(usgs_astro_url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    usgs_astro_products = soup.find('div', class_='collapsible results')
+
+    hemisphere_pages = usgs_astro_products.find_all('div', class_='item')
+
+    # List where everything will be stored
+    hemisphere_image_urls = []
+
+    for page in hemisphere_pages:
+        # Clear the dictionary for each iteration
+        hemisphere_dict = {}
+        
+        # Find the title and add it to the dictionary
+        title = page.find('div', class_='description').find('a').text
+        hemisphere_dict['title'] = title
+        
+        # Find the link to the detail page
+        hemisphere_link = page.find('div', class_='description').find('a')['href']
+        
+        # Visit the page and parse it
+        browser.visit(usgs_astro_url_split[0] + hemisphere_link)
+        hemisphere_html = browser.html
+        hemisphere_soup = BeautifulSoup(hemisphere_html, 'html.parser')
+        
+        # Find the link to the full image and add it to the dictionary
+        img_link = hemisphere_soup.find('div', class_='content').find('a')['href']
+        hemisphere_dict['img_url'] = img_link
+        
+        # Add the filled dictionary to the list
+        hemisphere_image_urls.append(hemisphere_dict)
+        
+        # Go a page back, ready for the next iteration
+        browser.back()
+        
+    hemisphere_image_urls
+    ###############################################################
+    # Close the browser after scraping
+    ###############################################################
+    browser.quit()
 
     ###############################################################
     # Put it all in a dictionary
@@ -146,11 +184,11 @@ def scraper():
         "news_title" : news_title,
         "news_p" : news_p,
         "featured_image_url" : featured_image_url,
-        "mars_html_table" : mars_html_table
+        "mars_html_table" : mars_html_table,
+        'hemisphere_image_urls' : hemisphere_image_urls
     }
 
-    # Close the browser after scraping
-    browser.quit()
-
+    ###############################################################
     # Return results
+    ############################################################### 
     return mars_data
